d787a7476329b0dcb9ee89d06baf96516fe58e89
fnachatoy <NachatoiFN@kadastr.ru>
2022-08-16 16:44:40 +0700

    bootstrap local

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :mygroups
+  root 'mygroups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
