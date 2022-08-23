3c3f2e2e30ee0b7eab2f6083f3ec4a597dbc5fac
wkfedor <74389434+wkfedor@users.noreply.github.com>
2022-08-24 03:05:28 +0700

    тест группы

--- app/controllers/loadfiles_controller.rb
+++ app/controllers/loadfiles_controller.rb
@@ -4,6 +4,9 @@ class LoadfilesController < ApplicationController
   # GET /loadfiles or /loadfiles.json
   def index
     @loadfiles = Loadfile.all
+
+    @myreg=@loadfiles.first.lfilename.read.to_s.force_encoding("UTF-8").scan(/@[a-z1-9]*/).uniq - ["@","@id"]
+
   end
 
   # GET /loadfiles/1 or /loadfiles/1.json
--- db/schema.rb
+++ db/schema.rb
@@ -32,12 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_031242) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "wfiles", force: :cascade do |t|
     t.string "word"
     t.string "flag"
