5c7a3dc8abc65e38b5c51c4418db6cc060ea12ea
fnachatoy <wkfedor@gmail.com>
2022-08-23 09:46:27 +0700

    план работ

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_184405) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "wfiles", force: :cascade do |t|
     t.string "word"
     t.string "flag"
