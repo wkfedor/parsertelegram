c5ab27b65e27cd11d6400d35dd42305db4062667
wkfedor <74389434+wkfedor@users.noreply.github.com>
2022-09-10 09:08:55 +0700

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -60,12 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_020507) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "wfiles", force: :cascade do |t|
     t.string "word"
     t.string "flag"
